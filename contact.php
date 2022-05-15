406238f7b0fa296cf56d45e0cd951cd44e2e796b
buhaymarella <marella.gutierrez84@gmail.com>
2022-05-15 18:38:55 +0800

    CONTACT WEBPAGE ADDED

---
+++ contact.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600;700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<title>website</title>
+</head>
+<body style="margin: 0; padding: 0; font-size: 14px">
+	<section class="banner" style=" height: 120vh;">
+		<nav>
+			<h1 style="color: white; font-size: 25px;">Travel Spot in Marinduque</h1>
+			<div class="nav-links" id="navLinks">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="about.php">About</a></li>
+					<li><a href="explore.php">Explore</a></li>
+					<li><a href="contact.php">Contacts</a></li>
+				</ul>
+			</div>
+		</nav>
+
+		<div class="cont">
+			<h1 style="margin-top: -50px; text-align: center; font-size: 30px;">Connect With Us</h1>
+			
+			<div class="contact-box"style=" background-color: whitesmoke; border-radius: 10px;">
+				<div class="contact-left">
+					<h3> Message Us Your Thoughts</h3>
+					<form>
+						<div class="input-row">
+							<div class="input-group">
+								<label>Name</label>
+								<input type="text" name="fname" placeholder="Your Name...">
+							</div>
+							<div class="input-group">
+								<label>Email</label>
+								<input type="email" name="email" placeholder="Your Email...">
+							</div>
+							<div class="input-group">
+								<label>Phone Number</label>
+								<input type="text" name="number" placeholder="Your Phone Number...">
+							</div>
+							<div class="input-group">
+								<label>Subject</label>
+								<input type="text" name="subject" placeholder="Subject...">
+							</div>
+						</div>
+
+						<label>Message</label>
+						<textarea rows="5" placeholder="Your Message Here"></textarea>
+
+						<button type="submit">SEND</button>
+					</form>
+				</div>
+				<div class="contact-right">
+					<h3>Contact Us</h3>
+						<table>
+							<tr>
+								<td>Email:</td>
+								<td>marinduque@gmail.com</td>
+							</tr>
+							<tr>
+								<td>website:</td>
+								<td>www.marinduquetravel.com.ph</td>
+							</tr>
+							<tr>
+								<td>Phone:</td>
+								<td>(+63) 966-1690-4201</td>
+							</tr>
+							<tr>
+								<td>Address:</td>
+								<td> Street no. 123, Brgy.----, sitio ---- <br>Santa Cruz, Marinduque- Philippines</td>
+							</tr>
+						</table>
+				</div>
+			</div>
+		</div>
+
+		<div class="footer" style="padding: 40px; margin-top: -10px;">
+			<p> &copy; 2022 Travel Marinduque <br> Santa Cruz, Marinduque - Philippines <br> Telephone no.(+63) 966-1690-4201. All Rights Reserved</p>
+
+
+			<a href="#"><img src="images/icon1.png"></a>
+			<a href="#"><img src="images/icon2.png"></a>
+			<a href="#"><img src="images/icon3.png"></a>
+			<a href="#"><img src="images/icon4.png"></a>
+		</div>
+
+</body>
+</html>
